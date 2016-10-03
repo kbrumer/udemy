610963413556cc36b2eed8cf2163b53a5edf4026
Ken Brumer <kbrumer@sosacorp.com>
2016-10-02 21:40:33 -0700

    Ok, half done.

--- CompleteReactNativeAndReduxCourse/albums/index.ios.js
+++ CompleteReactNativeAndReduxCourse/albums/index.ios.js
@@ -1,1 +1,17 @@
-// index.ios.js - place code in here for IOS
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText='Albums' />
+    <AlbumList />
+  </View>
+
+);
+
+// Render component to mobile device
+AppRegistry.registerComponent('albums', () => App);
