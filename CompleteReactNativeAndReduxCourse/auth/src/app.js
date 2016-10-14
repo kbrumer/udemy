b95dd872c34ca04180736edcc8d4ff703bbc3811
Ken Brumer <kbrumer@sosacorp.com>
2016-10-13 22:35:48 -0700

    redux.

--- CompleteReactNativeAndReduxCourse/auth/src/app.js
+++ CompleteReactNativeAndReduxCourse/auth/src/app.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+  // null: means we do not know the value
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBCxLo7n73J-2YtCItIN7EROTPs7vdDw0Y',
@@ -15,13 +18,35 @@ class App extends Component {
       storageBucket: 'authentication-deaef.appspot.com',
       messagingSenderId: '1082347566036'
     });
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch(this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
---
+++ CompleteReactNativeAndReduxCourse/tech_stack/SAMPLE.md
@@ -0,0 +1,47 @@
+reducers produce application state
+
+provider is communication glue with react
+
+
+
+This can be run here:
+
+https://stephengrider.github.io/JSPlaygrounds/
+
+>>>
+
+const reducer = (state = [], action) => {
+	if (action.type === 'split_string') {
+    return action.payload.split('');
+  } else if (action.type === 'add_character') {
+    return [ ...state, action.payload ];
+  }
+
+  return state;
+};
+
+const store = Redux.createStore(reducer);
+
+store.getState();
+
+// '0 >>>>';
+
+const action = {
+  type: 'split_string',
+  payload: 'asdf'
+};
+
+store.dispatch(action);
+
+'1 >>>>';
+
+store.getState();
+
+const action2 = {
+  type: 'add_character',
+  payload: 'a'
+};
+
+store.dispatch(action2);
+
+store.getState();
