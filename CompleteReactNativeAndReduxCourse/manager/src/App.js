4f9f825eee8cae4ea2ce2027e030902b3b3e1e1c
Ken Brumer <kbrumer@sosacorp.com>
2016-10-19 00:08:23 -0700

    Finished lecture 123

---
+++ CompleteReactNativeAndReduxCourse/manager/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import firebase from 'firebase';
+
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDaT3I71TWLONleu8EI_aFS8-viGnMMXBw',
+      authDomain: 'manager-a64e6.firebaseapp.com',
+      databaseURL: 'https://manager-a64e6.firebaseio.com',
+      storageBucket: 'manager-a64e6.appspot.com',
+      messagingSenderId: '922400156176'
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+    return (
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ CompleteReactNativeAndReduxCourse/manager/src/actions/types.js
@@ -0,0 +1,6 @@
+export const EMAIL_CHANGED = 'email_changed';
+export const PASSWORD_CHANGED = 'password_changed';
+
+export const LOGIN_USER_SUCCESS = 'login.user.success';
+export const LOGIN_USER_FAIL = 'login.user.fail';
+export const LOGIN_USER = 'login.user';
